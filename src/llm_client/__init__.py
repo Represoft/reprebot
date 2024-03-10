d4257968bf3c5b1fbfef7a23372a6794e2734149
este6an13 <diquintero@unal.edu.co>
2024-03-09 21:30:59 -0500

    reprebot/refac: #20 update setup model method to receive model type
    - add `model_type` argument which can be `gpt`, `hugging-face` or `fake`
    - note: currently `hugging-face` model cannot be use
      due to [issue](https://github.com/langchain-ai/langchain/issues/18639)
    - note: `FakeLLM` is not supported on Windows yet
    - replace client test with `TestFakeLLM` which should work in the
      pipeline without needing any API KEY or TOKEN, however won't work on Windows
    - add `clean-env.bat` script to re-install direct dependencies and
      remove non-used ones

--- src/llm_client/__init__.py
+++ src/llm_client/__init__.py
@@ -1,16 +1,31 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_contrib.llms.testing import FakeLLM
 
 class LLMClient:
-    def __init__(self, model_name="gpt-3.5-turbo-0125"):
-        self.api_key = os.environ["OPENAI_API_KEY"]
-        self.model_name = model_name
+    def __init__(self, model_type: str):
+        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
+        self.huggingfacehub_api_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+        self.model_type = model_type
         self.model = self.setup_model()
 
     def setup_model(self, temperature=0):
-        model = ChatOpenAI(
-            api_key=self.api_key,
-            model_name=self.model_name,
-            temperature=temperature
-        )
+        model = None
+        if self.model_type == "gpt":
+            model = ChatOpenAI(
+                api_key=self.openai_api_key,
+                model_name="gpt-3.5-turbo-0125",
+                temperature=temperature
+            )
+        # not supported on Windows
+        elif self.model_type == "fake":
+            model = FakeLLM()
+        """
+        elif self.model_type == "hugging-face":
+            llm = HuggingFaceEndpoint(
+                repo_id="google/gemma-7b",
+            )
+            model = ChatHuggingFace(llm=llm)
+        """
+        # https://github.com/langchain-ai/langchain/issues/18639
         return model
--- test/unit/src/test_llm_client.py
+++ test/unit/src/test_llm_client.py
@@ -1,17 +1,15 @@
-import os
 import pytest
 from src.llm_client import LLMClient
+from langchain_contrib.llms.testing import FakeLLM
 
-class TestLLMClient:
+class TestFakeLLM:
     @pytest.fixture
     def llm_client(self):
-        return LLMClient()
+        return LLMClient(model_type="fake")
 
     def test_init(self, llm_client):
-        assert llm_client.api_key == os.environ["OPENAI_API_KEY"]
-        assert llm_client.model_name == "gpt-3.5-turbo-0125"
+        assert llm_client.model_type == "fake"
 
     def test_setup_model(self, llm_client):
-        temperature = 0.5
-        model = llm_client.setup_model(temperature=temperature)
-        assert model.temperature == temperature
+        model = llm_client.setup_model()
+        assert isinstance(model, FakeLLM)
