a0ef7742a32bcfc9c6e6fb623f1b1c8c631067f8
este6an13 <diquintero@unal.edu.co>
2024-03-15 20:17:35 -0500

    reprebot/feat: #3 add hugging face model to llm client
    - use `ChatHuggingFace` to instantiate a `Hugging Face` models
    - use `HuggingFaceEndpoint` to connect to the `Hugging Face` model
    - add `hugging-face` to `LLMClient`
    - add `TestHuggingFaceLLMClient` unit test #20
    - update `langchain` to `0.1.12` version #23
    - install `transformers` dependency
    - install `Jinja2` dependency
    - add `HUGGINGFACEHUB_API_TOKEN` to `pipeline.yml`

--- test/unit/src/test_llm_client.py
+++ test/unit/src/test_llm_client.py
@@ -7,6 +7,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import FakeEmbeddings
 from langchain_core.runnables.base import RunnableSequence
 from langchain.chat_models.fake import FakeListChatModel
+from langchain_community.chat_models.huggingface import ChatHuggingFace
 
 
 class TestFakeLLMClient:
@@ -39,3 +40,34 @@ class TestFakeLLMClient:
         response = llm_client.query(user_input="")
         assert isinstance(response, str)
         assert response == "Hello"
+
+
+class TestHuggingFaceLLMClient:
+    @pytest.fixture
+    def llm_client(self):
+        return LLMClient(model_type="hugging-face")
+
+    def test_init(self, llm_client):
+        assert llm_client.model_type == "hugging-face"
+
+    def test_setup_model(self, llm_client):
+        model = llm_client.setup_model()
+        assert isinstance(model, ChatHuggingFace)
+
+    def test_setup_chain(self, llm_client):
+        # Empty retriever for testing
+        retriever = Chroma.from_documents(
+            documents=[Document(page_content="")],
+            embedding=FakeEmbeddings(size=1),
+        ).as_retriever(search_kwargs={"k": 1})
+        # Empty prompt for testing
+        prompt = ChatPromptTemplate.from_messages([""])
+        # Default "fake" model use in this test
+        model = llm_client.setup_model()
+        # RAG chain
+        chain = llm_client.setup_chain(retriever=retriever, prompt=prompt, model=model)
+        assert isinstance(chain, RunnableSequence)
+
+    def test_query(self, llm_client):
+        response = llm_client.query(user_input="")
+        assert isinstance(response, str)
