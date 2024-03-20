c19eeb3ecefa806e67fd275d1cf2920ace884616
este6an13 <diquintero@unal.edu.co>
2024-03-19 23:06:17 -0500

    reprebot/refac: #30 use fp in llm client module
    - refactor `llm_client` module to use FP instead of OOP
    - refactor `test_llm_client` module to use FP instead of OOP

--- src/llm_client/__init__.py
+++ src/llm_client/__init__.py
@@ -10,54 +10,44 @@ from langchain.chat_models.fake import FakeListChatModel
 from langchain_community.chat_models.huggingface import ChatHuggingFace
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 
-class LLMClient:
-    def __init__(self, model_type: str):
-        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
-        self.huggingfacehub_api_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
-        self.model_type = model_type
-        self.model = self.setup_model()
-
-    def setup_model(self, temperature=0):
-        model = None
-        if self.model_type == "gpt":
-            model = ChatOpenAI(
-                api_key=self.openai_api_key,
-                model_name="gpt-3.5-turbo-0125",
-                temperature=temperature
-            )
-        elif self.model_type == "fake":
-            model = FakeListChatModel(responses=["Hello",])
-        elif self.model_type == "hugging-face":
-            llm = HuggingFaceEndpoint(
-                repo_id="google/gemma-7b",
-            )
-            model = ChatHuggingFace(llm=llm)
-        return model
-
-    def setup_chain(self, retriever, prompt, model):
-        chain = (
-            {
-                "input": RunnablePassthrough(),
-                "context": retriever,
-            }
-            | prompt
-            | model
-            | StrOutputParser()
+def setup_model(model_type: str, temperature: float = 0):
+    model = None
+    if model_type == "gpt":
+        model = ChatOpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+            model_name="gpt-3.5-turbo-0125",
+            temperature=temperature
         )
-        return chain
-
-    def query(self, user_input: str):
-        # Empty retriever for testing
-        retriever = Chroma.from_documents(
-            documents=[Document(page_content="")],
-            embedding=FakeEmbeddings(size=1),
-        ).as_retriever(search_kwargs={"k": 1})
-        # Empty prompt for testing
-        prompt = ChatPromptTemplate.from_messages([""])
-        chain = self.setup_chain(
-            retriever=retriever,
-            prompt=prompt,
-            model=self.model
+    elif model_type == "fake":
+        model = FakeListChatModel(responses=["Hello"])
+    elif model_type == "hugging-face":
+        llm = HuggingFaceEndpoint(
+            repo_id="google/gemma-7b",
         )
-        response = chain.invoke(user_input)
-        return response
+        model = ChatHuggingFace(llm=llm)
+    return model
+
+def setup_chain(retriever, prompt, model):
+    chain = (
+        {
+            "input": RunnablePassthrough(),
+            "context": retriever,
+        }
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+    return chain
+
+def query(user_input: str, model_type: str):
+    # Empty retriever for testing
+    retriever = Chroma.from_documents(
+        documents=[Document(page_content="")],
+        embedding=FakeEmbeddings(size=1),
+    ).as_retriever(search_kwargs={"k": 1})
+    # Empty prompt for testing
+    prompt = ChatPromptTemplate.from_messages([""])
+    model = setup_model(model_type)
+    chain = setup_chain(retriever=retriever, prompt=prompt, model=model)
+    response = chain.invoke(user_input)
+    return response
