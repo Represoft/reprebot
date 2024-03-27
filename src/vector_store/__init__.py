b314bf891e2c564440a19dd04840c04fb93a3b8b
este6an13 <diquintero@unal.edu.co>
2024-03-27 11:41:48 -0500

    reprebot/refac: #5 add vector db path to vector store module
    - add `vector_db_path` to `setup_vector_database` function
    - handle zero documents case

--- src/vector_store/__init__.py
+++ src/vector_store/__init__.py
@@ -9,25 +9,36 @@ import shutil
 from langchain_core.vectorstores import VectorStoreRetriever
 
 def load_documents(path: str) -> List[Document]:
-    loader = TextLoader()
-    documents = loader.load(path)
+    loader = TextLoader(path)
+    documents = loader.load()
     return documents
 
 
 def chunk_documents(documents: List[Document]) -> List[Document]:
-    splitter = CharacterTextSplitter()
-    chunked_documents = [splitter.split(doc) for doc in documents]
+    text_splitter = CharacterTextSplitter()
+    chunked_documents = text_splitter.split_documents(documents)
+    if len(chunked_documents) == 0:
+        chunked_documents = [Document(page_content="")]
     return chunked_documents
 
 
 def setup_vector_database(path: str) -> Chroma:
+    vector_db = None
+    vector_db_path = "vectordb/"
     if not os.path.exists(path):
         documents = load_documents(path)
         chunked_documents = chunk_documents(documents)
-        vector_db = Chroma.from_documents(chunked_documents, FakeEmbeddings())
-        vector_db.persist(path)
+        vector_db = Chroma.from_documents(
+            documents=chunked_documents,
+            embedding=FakeEmbeddings(),
+            persist_directory=vector_db_path,
+        )
+        vector_db.persist(vector_db_path)
     else:
-        vector_db = Chroma(persist_directory=path, embeddings=FakeEmbeddings())
+        vector_db = Chroma(
+            persist_directory=path,
+            embedding_function=FakeEmbeddings(),
+        )
     return vector_db
 
 
