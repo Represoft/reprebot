e474f1ecff5200d980f1ec72ec18f2eef5f8826f
este6an13 <diquintero@unal.edu.co>
2024-04-03 00:01:12 -0500

    reprebot/fix: #5 fix issue with vector db not retrieving docs
    - the vector db was not working for me because I had a cached
      version that didn't include any document
    - removing the `vectordb` folder and running the script again
      fixed the issue
    - a `main.py` file with some example queries was added for
      users to try some questions

--- src/vector_store/__init__.py
+++ src/vector_store/__init__.py
@@ -1,6 +1,6 @@
 from typing import List
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain_community.vectorstores import Chroma
 import os
@@ -32,7 +32,7 @@ def load_documents_from_folders() -> List[Document]:
 
 
 def chunk_documents(documents: List[Document]) -> List[Document]:
-    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter()
     chunked_documents = text_splitter.split_documents(documents)
     if len(chunked_documents) == 0:
         chunked_documents = [Document(page_content="")]
@@ -45,7 +45,7 @@ def start_vector_database(chunked_documents, embedding_function) -> Chroma:
         embedding=embedding_function,
         persist_directory=VECTOR_DATABASE_PATH,
     )
-    vector_db.persist(VECTOR_DATABASE_PATH)
+    vector_db.persist()
     return vector_db
 
 
@@ -70,7 +70,7 @@ def setup_empty_retriever(embedding_function) -> VectorStoreRetriever:
         embedding=embedding_function,
     )
     retriever = vector_db.as_retriever(search_kwargs={"k": 1})
-    return retriever, vector_db
+    return retriever
 
 
 def setup_full_retriever(embedding_function) -> VectorStoreRetriever:
@@ -82,7 +82,7 @@ def setup_full_retriever(embedding_function) -> VectorStoreRetriever:
     vector_db = setup_vector_database(chunked_documents, embedding_function)
     # generate retriever
     retriever = vector_db.as_retriever()
-    return retriever, vector_db
+    return retriever
 
 
 def setup_embeddings(config: VectorStoreConfig):
