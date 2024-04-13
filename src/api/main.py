72a3176cbe8e1cf741147025e50308f9345b9196
este6an13 <diquintero@unal.edu.co>
2024-04-13 00:34:03 -0500

    reprebot/feat: #5 add vector store get endpoints to api
    - add `document?filename` endpoint to api
    - add `document?document_id` endpoint to api
    - rename query endpoint to be `query?q`

--- src/api/main.py
+++ src/api/main.py
@@ -1,26 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import sys
 sys.path.append('../..')
 from src.llm_client import query as _query
 from src.llm_client.types import GPTModelConfig
 from src.vector_store.types import VectorStoreConfig
+from src.vector_store import get_document_by_filename as _get_document_by_filename
+from src.vector_store import get_document_by_id as _get_document_by_id
+
 
 app = FastAPI()
+
 
 model_config = GPTModelConfig(
     model_name="gpt-3.5-turbo-0125",
 )
+
 
 vector_store_config = VectorStoreConfig(
     embeddings="OPENAI",
     retriever="FULL",
 )
 
-@app.get("/{query}")
-async def query(query):
-    response = _query(
-        user_input=query,
-        model_config=model_config,
-        vector_store_config=vector_store_config
-    )
+
+@app.get("/query")
+async def query(q: str = Query(None)):
+    if q:
+        response = _query(
+            user_input=q,
+            model_config=model_config,
+            vector_store_config=vector_store_config
+        )
+    else:
+        response = {
+            "error": "'query' parameter must be provided."
+        }
+    return response
+
+
+@app.get("/document")
+async def document(filename: str = Query(None), document_id: str = Query(None)):
+    if filename:
+        response = _get_document_by_filename(filename)
+    elif document_id:
+        response = _get_document_by_id(document_id)
+    else:
+        response = {
+            "error": "Either 'filename' or 'document_id' query parameter must be provided."
+        }
     return response
