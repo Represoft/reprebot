f15e9b77a3ecea467cd05d457a7dff69f15028b7
este6an13 <diquintero@unal.edu.co>
2024-04-13 00:58:30 -0500

    reprebot/fix: #34 fix codacy code quality issues
    - remove string concatenation in sql query
    - rename `ìd` parameter

--- src/api/main.py
+++ src/api/main.py
@@ -4,8 +4,8 @@ sys.path.append('../..')
 from src.llm_client import query as _query
 from src.llm_client.types import GPTModelConfig
 from src.vector_store.types import VectorStoreConfig
-from src.vector_store import get_document_by_filename as _get_document_by_filename
-from src.vector_store import get_document_by_id as _get_document_by_id
+from src.vector_store import get_document_by_filename
+from src.vector_store import get_document_by_id
 
 
 app = FastAPI()
@@ -40,9 +40,9 @@ async def query(q: str = Query(None)):
 @app.get("/document")
 async def document(filename: str = Query(None), document_id: str = Query(None)):
     if filename:
-        response = _get_document_by_filename(filename)
+        response = get_document_by_filename(filename)
     elif document_id:
-        response = _get_document_by_id(document_id)
+        response = get_document_by_id(document_id)
     else:
         response = {
             "error": "Either 'filename' or 'document_id' query parameter must be provided."
