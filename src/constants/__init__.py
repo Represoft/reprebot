689b10d9ac8bd4b77740aa3ffaa9c0ecd43eed87
Aliriio <amartinezba@unal.edu.co>
2024-05-06 22:00:46 -0500

    reprebot/feat: #84 implement context builder script for Student Requests
    - Implemented the context builder script to extract and store information from the website 'Solicitudes Estudiantiles - Secretaría de Facultad'
    - Create and customize the 'extract_texts' function to divide the extracted information into multiple text files based on sections

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,25 +1,22 @@
 from pathlib import Path
 import os
 
-PROJECT_ROOT = str(Path(__file__).parent.parent.parent)
-
+PROJECT_ROOT = str(Path(__file__).parent.parent)
 
 CONTEXT_DATA_GROUPS = [
     "faculty_secretary_faq",
+    "faculty_secretary_students_requests",
 ]
 
-
 CONTEXT_DATA_PATHS = {
-    "faculty_secretary_faq": os.path.join(
-        PROJECT_ROOT, "data/faculty_secretary_faq/"
-    )
+    "faculty_secretary_faq": os.path.join(PROJECT_ROOT, "data/faculty_secretary_faq/"),
+    "faculty_secretary_students_requests": os.path.join(PROJECT_ROOT, "data/faculty_secretary_students_requests/"),
 }
-
 
 CONTEXT_DATA_SOURCES = {
-    "faculty_secretary_faq": "https://ingenieria.bogota.unal.edu.co/es/dependencias/secretaria-academica/preguntas-frecuentes.html"
+    "faculty_secretary_faq": "https://ingenieria.bogota.unal.edu.co/es/dependencias/secretaria-academica/preguntas-frecuentes.html",
+    "faculty_secretary_students_requests": "https://ingenieria.bogota.unal.edu.co/es/dependencias/secretaria-academica/solicitudes-estudiantiles.html",
 }
-
 
 VECTOR_DATABASE_PATH = os.path.join(PROJECT_ROOT, "vectordb/")
 DATABASE_PATH = os.path.join(PROJECT_ROOT, "reprebot.db")
