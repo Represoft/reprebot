11e2e3f3a5359262e7eb3696e7bdcbd2fef19dbb
este6an13 <diquintero@unal.edu.co>
2024-07-21 23:30:49 -0500

    reprebot/feat: #6 add conversations log feature
    - add `conversation` endpoint to api
    - add `log` module to store records to database
    - update `database` module to include a `conversations` table
    - rename `database` methods to identify document and conversations operations
    - add `push_conversation_log_record` method to `database`
    - add `get_conversation_log` method to `database`
    - add methods to reset individual tables of the database
    - add `generate_conversations_log` script
    - update `crud` module to use renamed documents operations
    - update `vector_store` module to use renamed documents operations
    - update `.gitignore` to ignore `conversations_log.json` file
    - update architecture diagram in `README.md` file
    - update architecture information in `README.md` file
    - update app to use new `conversation` endpoint
    - add instructions to generate the conversations log to the `README.md` file

---
+++ src/scripts/generate_conversations_log.py
@@ -0,0 +1,35 @@
+import sys
+import json
+from datetime import datetime
+
+sys.path.append("../..")
+from src.database import Database
+from src.constants import DATABASE_PATH
+
+def generate_json_log(records, filename="conversations_log.json"):
+    data = []
+    for record in records:
+        # Convert timestamp to string if it's a datetime object
+        record = list(record)
+        if isinstance(record[3], datetime):
+            record[3] = record[3].isoformat()
+        # Append the record as a dictionary
+        data.append({
+            "ID": record[0],
+            "Question": record[1],
+            "Answer": record[2],
+            "Timestamp": record[3],
+            "Conversation ID": record[4]
+        })
+
+    # Write data to JSON file
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
+
+    print(f"JSON log generated: {filename}")
+
+database = Database(db_name=DATABASE_PATH)
+records = database.get_conversations_log()
+generate_json_log(records)
+
+print(f"Total records: {len(records)}")
