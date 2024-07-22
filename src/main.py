ae28aa11b62dac6878da0bd1ac7ba68e4130eea3
este6an13 <diquintero@unal.edu.co>
2024-07-21 20:28:37 -0500

    reprebot/docs: #56 update readme file
    - add instructions to set `OPENAI_API_KEY` env variable
    - add instructions to run demo questions script
    - complete instructions to run context builder
    - add note about `API_HOST` env variable
    - add comment on vector store setup
    - add instructions to run vector store setup script
    - add instructions to un vector store reset script
    - organize demo questions script

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+"""
+This is a demo script to test the `OPENAI_API_KEY` with four demo questions.
+"""
+
 import sys
 
 sys.path.append("..")
@@ -14,46 +18,56 @@ vector_store_config = VectorStoreConfig(
     retriever="FULL",
 )
 
-response = query(
-    user_input="""
-        ¿Hasta qué fecha se puede solicitar cancelación
-        de asignaturas sin pérdida de créditos?
-    """,
+question_1 = """
+    ¿Hasta qué fecha se puede solicitar cancelación
+    de asignaturas sin pérdida de créditos?
+"""
+
+response_1 = query(
+    user_input=question_1,
     model_config=model_config,
     vector_store_config=vector_store_config,
 )
 
-print(response, "\n")
+print("QUESTION 1:", question_1, "\n")
+print("RESPONSE 1:", response_1, "\n\n")
 
-response = query(
-    user_input="""
-        ¿Cómo se accede al beneficio de descuento electoral del 10 % aplicable
-        sobre el valor de la matrícula?
-    """,
+question_2 = """
+    ¿Cómo se accede al beneficio de descuento electoral del 10 % aplicable
+    sobre el valor de la matrícula?
+"""
+
+response_2 = query(
+    user_input=question_2,
     model_config=model_config,
     vector_store_config=vector_store_config,
 )
 
-print(response, "\n")
+print("QUESTION 2:", question_2, "\n")
+print("RESPONSE 2:", response_2, "\n\n")
 
+question_3 = """
+    ¿Cuándo puedo cancelar materias? Tienes enlaces relacionados con este proceso?
+"""
 
-response = query(
-    user_input="""
-        ¿Cuándo puedo cancelar materias? Tienes enlaces relacionados con este proceso?
-    """,
+response_3 = query(
+    user_input=question_3,
     model_config=model_config,
     vector_store_config=vector_store_config,
 )
 
-print(response, "\n")
+print("QUESTION 3:", question_3, "\n")
+print("RESPONSE 3:", response_3, "\n\n")
 
+question_4 = """
+    ¿Cuál es la diferencia entre práctica y pasantía?
+"""
 
-response = query(
-    user_input="""
-        ¿Cuál es la diferencia entre práctica y pasantía?
-    """,
+response_4 = query(
+    user_input=question_4,
     model_config=model_config,
     vector_store_config=vector_store_config,
 )
 
-print(response, "\n")
+print("QUESTION 4:", question_4, "\n")
+print("RESPONSE 4:", response_4, "\n\n")
